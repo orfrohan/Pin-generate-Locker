1c3e589b5b2169cb698dd83aada30433ac61b3b5
orfrohan <soikotrohan08@gmail.com>
2022-03-03 13:52:49 +0600

    locker type

---
+++ script.js
@@ -0,0 +1,49 @@
+// generate pin
+function getPin() {
+  const pin = Math.round(Math.random() * 10000);
+  const pinString = pin + "";
+  if (pinString.length == 4) {
+    return pin;
+  } else {
+    // console.log('got 3 digit and calling again', pin);
+    return getPin();
+  }
+}
+function generatePin() {
+  const pin = getPin();
+  document.getElementById("display-pin").value = pin;
+}
+
+// match pin part
+
+let button = Array.from(document.querySelectorAll(".button"));
+let calInput = document.querySelector("#typed-number");
+button.map((button) => {
+  button.addEventListener("click", function (e) {
+    switch (e.target.innerText) {
+      case "C":
+        calInput.value = "";
+        break;
+      case "<":
+        calInput.value = calInput.value.slice(0, -1);
+        break;
+      default:
+        calInput.value += e.target.innerText;
+        break;
+    }
+    return;
+  });
+});
+
+// verify part
+function verifyPin() {
+  const pin = document.querySelector("#display-pin").value;
+  const typedNumbers = document.querySelector("#typed-number").value;
+  let successMessage = document.querySelector(".notify-success");
+  let failError = document.querySelector(".notify-fail");
+  if (pin == typedNumbers) {
+    successMessage.style.display = "block";
+  } else {
+    failError.style.display = "block";
+  }
+}
